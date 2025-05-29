ed261c58b59b8ec3ebbbbfe555755e4f8e5461ac
Andrew Hill <github@andrewh.space>
2025-05-29 15:26:11 +0100

    add pxctl.sh

---
+++ assets/pxctl.sh
@@ -0,0 +1,5 @@
+NAMESPACE=$(kubectl get stc -A -o jsonpath='{.items[].metadata.namespace}')
+POD=$(kubectl get pods -n $NAMESPACE -l name=portworx -o wide | tail -1 | cut -f 1 -d " ")
+ADMIN_TOKEN=$(kubectl -n $NAMESPACE get secret px-admin-token -o jsonpath='{.data.auth-token}' 2>/dev/null | base64 -d)
+[ "$ADMIN_TOKEN" ] && kubectl -n $NAMESPACE exec -ti $POD -c portworx -- /opt/pwx/bin/pxctl context create admin --token=$ADMIN_TOKEN
+kubectl -n $NAMESPACE exec -ti $POD -c portworx -- /opt/pwx/bin/pxctl $@
